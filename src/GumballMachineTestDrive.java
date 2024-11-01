521ea69447c22c442c709d017a7ade16724dd876
nawadream <644259029@webmail.npru.ac.th>
2024-11-01 22:24:58 +0700

    .

---
+++ src/GumballMachineTestDrive.java
@@ -0,0 +1,13 @@
+public class GumballMachineTestDrive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GumballMachine gumballMachine = new GumballMachine(3);
+		System.out.println(gumballMachine);
+		//gumballMachine.turnCrank();
+		
+		
+	
+	}
+
+}
